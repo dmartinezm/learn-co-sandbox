b01b09c916ff1772a505b58fd009b2201a9a7784
Diego Martinez <dmartinezm@gmail.com>
2019-09-22 19:28:53 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,18 @@
+def stylish_chef
+  best_hairstyle = "Guy Fieri"
+  return "Martha Stewart"
+  "Guy Fieri"
+end
+
+stylish_chef
+
+name ="Andrew "
+def greeting (name="ashley")
+  puts "hello #{name}"
+end
+
+greeting("Diego")
+greeting(name)
+greeting()
+greeting("sonia")
+
---
+++ learning-comments.rb
@@ -0,0 +1,18 @@
+chance_of_rain = 0.76
+
+puts "Let's go outside!"
+if chance_of_rain<= 0.25
+    puts "Pack an umbrella!"
+  elsif (chance_of_rain>0.25 && chance_of_rain< 0.75)
+    puts "Pack boots"
+  else
+    puts "Enjoy the fine day!"
+end
+puts "Oh, and always wear sunscreen!"
+
+this_year = 2018
+puts "You know what year it is?" unless
+this_year == 2019
+
+
+
